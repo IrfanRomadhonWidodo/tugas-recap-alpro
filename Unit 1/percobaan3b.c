68b61dc31dda6ae7825f10af4cc64a49282711ad
IrfanRomadhonWidodo <161714829+IrfanRomadhonWidodo@users.noreply.github.com>
2024-03-02 13:05:01 +0000

    percobaan3b.c

--- Unit 1/percobaan3b.c
+++ Unit 1/percobaan3b.c
@@ -4,4 +4,8 @@ int main() {
   printf("Oh my, oh my God \n???? ? \nI was really hoping that he will come through");
   return 0;
 }
-// "\n" digunakan untuk mengganti baris, ibaratnya enter dalam keyboard
+/* fungsi dari \n adalah membuat new line atau dalam sebuah string, itu menandakan bahwa teks setelahnya akan dimulai di baris baru atau pada posisi awal baris berikutnya.
+output yang dihasilkan:
+Oh my, oh my God
+∞ÿê∞âüφûê∞û┤ δéÿ
+I was really hoping that he will come through */

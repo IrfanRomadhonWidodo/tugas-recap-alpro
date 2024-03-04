d51cb3459f5758f6fd1cd70fa2a523cc2097a4b7
IrfanRomadhonWidodo <161714829+IrfanRomadhonWidodo@users.noreply.github.com>
2024-03-04 12:28:36 +0000

    fixed unit 3

--- Unit 3/percobaan4.c
+++ Unit 3/percobaan4.c
@@ -16,3 +16,8 @@ int main() {
 
     return 0;
 }
+/*prorgam tersebut bekerja pertama dari pendefinisian max person 3 dan max name length 20
+lalu kita membuath array char names dengan panjang 3 dan panjang char 20
+lanjut array ages dengan panjang 3
+print name tab 2x age
+lalu for untuk mengulang sebanyak 3x mencetak nama dan ages*/

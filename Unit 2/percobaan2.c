b75c1bf0024687d0cf723bf605668aa0f8d2a842
IrfanRomadhonWidodo <161714829+IrfanRomadhonWidodo@users.noreply.github.com>
2024-03-04 12:01:36 +0000

    fixed unit 2

--- Unit 2/percobaan2.c
+++ Unit 2/percobaan2.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
 
-int angka;
-
 int main() {
-    
+    int angka;
+
     printf("Masukan angka: \n");
     scanf("%d", &angka);
 
-    if (angka > 0) {
-        printf("Kamu baru saja memasukan bilangan positif.\n");
-    } else if (angka < 0) {
-        printf("Kamu baru saja memasukan bilangan negatif.\n");
-    } else {
-        printf("Kamu baru saja memasukan nol.\n");
-    }
+    (angka > 0) ? printf("Kamu baru saja memasukan bilangan positif.\n") : (angka < 0) ? printf("Kamu baru saja memasukan bilangan negatif.\n") : printf("Kamu baru saja memasukan nol.\n");
 
     return 0;
 }

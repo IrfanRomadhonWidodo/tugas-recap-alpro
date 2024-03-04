30fe64a533785bc01f660391f20f55d721f4c716
IrfanRomadhonWidodo <161714829+IrfanRomadhonWidodo@users.noreply.github.com>
2024-03-04 12:42:35 +0000

    fixed unit 2

--- Unit 2/percobaan6b.c
+++ Unit 2/percobaan6b.c
@@ -18,3 +18,8 @@ int main() {
 
     return 0;
 }
+
+/*Cara kerjanya adalah dengan menggunakan nested do-while loop untuk mengalikan bilangan utama dengan bilangan pengali. 
+Perhitungan dimulai dengan melakukan perkalian terlebih dahulu sebelum melakukan perbandingan. 
+Setelah loop dalam selesai dijalankan, nilai bilangan pengali direset kembali ke 1 untuk mengulang perkalian dengan bilangan utama berikutnya. 
+Setelah itu, hasil perkalian ditampilkan ke layar.*/

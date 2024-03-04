58bd29421ede16a67c412f32eb8a3eb36765e508
IrfanRomadhonWidodo <161714829+IrfanRomadhonWidodo@users.noreply.github.com>
2024-03-04 12:31:12 +0000

    fixed unit 2

--- Unit 2/percobaan4b.c
+++ Unit 2/percobaan4b.c
@@ -17,4 +17,16 @@ int main(){
     return 0;
 }
 
-/*program */
+/*
+Loop pertama akan berjalan dua kali, karena nilai awal i adalah 2 dan batas atasnya adalah 3. Setiap kali loop berjalan, nilai i akan bertambah satu (++i).
+Loop kedua akan berjalan dua kali untuk setiap iterasi dari loop pertama. Nilai awal j adalah 1 dan batas atasnya adalah 2. Setiap kali loop berjalan, nilai j akan bertambah satu (++j).
+Setiap iterasi loop kedua akan mencetak nilai j sebagai "Bilangan pengali" dan hasil perkalian dari i * j sebagai "Hasil perkalian".
+program ini mencetak hasil perkalian dari bilangan utama (2 dan 3) dengan bilangan pengali (1 dan 2).
+ketika i = 2, bilangan utama = 2. loop kedua berjalan :
+saat j = 1 bilangan pengali = j = 1,  Hasil Perkalian = j*i = 2*1 = 2.
+saat j = 2 bilangan pengali = j = 2,  Hasil Perkalian = j*i = 2*2 = 4.
+
+ketika i = 3, bilangan utama = 3. loop kedua berjalan :
+saat j = 1 bilangan pengali = j = 1,  Hasil Perkalian = j*i = 3*1 = 3.
+saat j = 2 bilangan pengali = j = 2,  Hasil Perkalian = j*i = 3*2 = 6.
+*/
